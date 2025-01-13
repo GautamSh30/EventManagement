763f11f3e84635061e4c79cc6a3155fae3414a60
Gautam Sharma <gautam2586272@gmail.com>
2025-01-13 11:53:05 +0530

    backend initial setup

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import morgan from 'morgan'
+import connect from './db/db.js';
+
+const app = express();
+connect();
+
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+export default app;
---
+++ server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config.js';
+import http from 'http';
+import app from './app.js'
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`)
+})
